70900fa6bfd1f1a71d9fc442d347912604d6266a
bede abbe <bede.abbe91@gmail.com>
2023-04-11 17:11:24 +0000

    db password change

--- tourismBackend/configs/database.js
+++ tourismBackend/configs/database.js
@@ -1,11 +1,13 @@
 const mysql = require('mysql')
 
 const connection = mysql.createConnection({
-  // host: '172.27.51.200',
-  socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
   host: 'localhost',
-  user: 'terminal',
-  password: 'T34m1na!2@123',
+  // socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
+  host: 'localhost',
+  // user: 'terminal',
+  // password: 'T34m1na!2@123',
+  user: 'root',
+  password: 'Qwerty@234zxcv',
   database: 'tourism',
   port: '3306'
 })
