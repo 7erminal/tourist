e2dea766335469ecb442d64cc59bb20926f59c2d
bede abbe <bede.abbe91@gmail.com>
2023-04-14 22:46:22 +0000

    url change

--- tourismBackend/configs/database.js
+++ tourismBackend/configs/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql')
 const connection = mysql.createConnection({
   host: 'localhost',
   // socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
-  host: 'localhost',
+  // host: 'localhost',
   // user: 'terminal',
   // password: 'T34m1na!2@123',
   user: 'root',
