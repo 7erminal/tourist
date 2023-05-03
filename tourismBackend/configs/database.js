062664e10d9e259591d061711083b93ef432fa6b
bede abbe <bede.abbe91@gmail.com>
2023-05-03 09:02:14 +0000

    DB change to prod

--- tourismBackend/configs/database.js
+++ tourismBackend/configs/database.js
@@ -2,12 +2,12 @@ const mysql = require('mysql')
 
 const connection = mysql.createConnection({
   host: 'localhost',
-  socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
+  // socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
   // host: 'localhost',
-  user: 'terminal',
-  password: 'T34m1na!2@123',
-  // user: 'root',
-  // password: 'Qwerty@234zxcv',
+  // user: 'terminal',
+  // password: 'T34m1na!2@123',
+  user: 'root',
+  password: 'Qwerty@234zxcv',
   database: 'tourism',
   port: '3306'
 })
